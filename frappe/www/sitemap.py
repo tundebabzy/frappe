7c13712435aafa2b9541de5513ac46e0222718c8
tunde <tundebabzy@gmail.com>
2017-05-16 10:39:55 +0100

    adds py2 and py3 support for dict methods

--- frappe/www/sitemap.py
+++ frappe/www/sitemap.py
@@ -16,17 +16,32 @@ def get_context(context):
 	"""generate the sitemap XML"""
 	host = get_request_site_address()
 	links = []
-	for route, page in get_pages().iteritems():
-		if not page.no_sitemap:
-			links.append({
-				"loc": urllib.basejoin(host, urllib.quote(page.name.encode("utf-8"))),
-				"lastmod": nowdate()
-			})
+	try:
+		for route, page in get_pages().iteritems():
+			if not page.no_sitemap:
+				links.append({
+					"loc": urllib.basejoin(host, urllib.quote(page.name.encode("utf-8"))),
+					"lastmod": nowdate()
+				})
+	except AttributeError:
+		for route, page in get_pages().items():
+			if not page.no_sitemap:
+				links.append({
+					"loc": urllib.basejoin(host, urllib.quote(page.name.encode("utf-8"))),
+					"lastmod": nowdate()
+				})
 
-	for route, data in get_all_page_context_from_doctypes().iteritems():
-		links.append({
-			"loc": urllib.basejoin(host, urllib.quote((route or "").encode("utf-8"))),
-			"lastmod": get_datetime(data.get("modified")).strftime("%Y-%m-%d")
-		})
+	try:
+		for route, data in get_all_page_context_from_doctypes().iteritems():
+			links.append({
+				"loc": urllib.basejoin(host, urllib.quote((route or "").encode("utf-8"))),
+				"lastmod": get_datetime(data.get("modified")).strftime("%Y-%m-%d")
+			})
+	except AttributeError:
+		for route, data in get_all_page_context_from_doctypes().items():
+			links.append({
+				"loc": urllib.basejoin(host, urllib.quote((route or "").encode("utf-8"))),
+				"lastmod": get_datetime(data.get("modified")).strftime("%Y-%m-%d")
+			})
 
 	return {"links":links}
